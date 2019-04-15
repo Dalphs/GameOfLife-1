bc4bfcc2124e028b55028e20878ff43801bc6aab
sainsh <kfsainsh@gmail.com>
2019-04-15 12:09:51 +0200

    added 2 buttons to game.fxml, one to stop the simulation and one for creating a new simulation. added a timer to run the simulation, added a onMouseClicked ovent for each Cell to switch Alive added update method which first loops through the grid an updates the amount of neighbors the cells have    and then calls update on each cell

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,14 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Controller {
@@ -12,44 +17,129 @@ public class Controller {
     private int size = 5;
     private int gridSize = 100;
 
+    private Rectangle[][] grid;
+
+
     private Game game;
+
+    private Timer timer;
+    private TimerTask task;
 
     @FXML
     private AnchorPane cellPane;
 
-    public Controller() {
 
-
+    @FXML
+    public void initialize() {
 
         game = new Game();
+        grid = new Rectangle[gridSize][gridSize];
 
-    }
-
-    public void startSim() {
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
 
-                Rectangle rect = new Rectangle(size*i,size*j,size,size);
+                Rectangle rect = new Rectangle(size * i, size * j, size, size);
+                rect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        onCellClicked(event.getX(), event.getY());
+                    }
+                });
+
+                rect.fillProperty().setValue(fill(i, j));
+
+                grid[i][j] = rect;
+                cellPane.getChildren().add(rect);
+            }
+
+        }
+    }
+
+    private void onCellClicked(double x, double y) {
+
+        int gridX = (int) (x / size);
+        int gridY = (int) (y / size);
+
+        game.getBoard()[gridX][gridY].setAlive(!game.getBoard()[gridX][gridY].isAlive());
+        grid[gridX][gridY].fillProperty().setValue(fill(gridX,gridY));
+    }
 
 
+    public void startSim() {
 
-                if (game.getBoard()[i][j].isAlive()) {
-                    rect.fillProperty().setValue(Color.RED);
-                } else {
-                    rect.fillProperty().setValue(Color.GRAY);
+        timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                update();
+            }
+        };
+        timer.scheduleAtFixedRate(task, 1000, 1000);
+
+    }
+
+    private Color fill(int i, int j) {
+
+        if (game.getBoard()[i][j].isAlive()) {
+            return Color.RED;
+        } else {
+            return Color.GRAY;
+        }
+    }
+
+    public void update() {
+
+        for (int i = 0; i < game.getBoard().length; i++) {
+
+            for (int j = 0; j < game.getBoard()[0].length; j++) {
+
+                int neighbors = 0;
+                try {
+                    for (int m = -1; m < 2; m++) {
+                        for (int n = -1; n < 2; n++) {
+
+                            if (game.getBoard()[i + m][j + n].isAlive()) {
+                                neighbors++;
+                            }
+
+                        }
+                    }
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    continue;
                 }
 
-                cellPane.getChildren().add(rect);
+
+                game.getBoard()[i][j].setNeighbors(neighbors);
+
             }
 
         }
 
 
+        for (int i = 0; i < grid.length; i++) {
+
+            for (int j = 0; j < grid[0].length; j++) {
+
+                game.getBoard()[i][j].update();
+
+                grid[i][j].fillProperty().setValue(fill(i, j));
+
+            }
+        }
+
     }
 
-    public void update(){
 
+    public void stopSim() {
+
+        timer.cancel();
+        task.cancel();
     }
 
+    public void newSim() {
 
+        cellPane.getChildren().removeAll();
+
+        initialize();
+    }
 }
